f75eef098d9c93e776106be7ab981c286b6b32ef
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 19:14:12 +0100

    study: extruturas de interacao - while

--- interaction.rb
+++ interaction.rb
@@ -2,12 +2,25 @@
 
 #leia um array de elementos - linguagens de programacao
 
-linguagens = ["Ruby", "Go", "JavaScript"] 
-count = 1
-#intercao na lista com  laco for 
+# linguagens = ["Ruby", "Go", "JavaScript"] 
+# count = 1
+# #intercao na lista com  laco for 
 
-for linguagem in linguagens
-    puts linguagem
-    puts count 
-    count = count +1
+# for linguagem in linguagens
+#     puts linguagem
+#     puts count 
+#     count = count +1
+# end
+
+
+#WHILE
+
+#comparar um numero com uma condicao 
+#imprima todos os numeros maiores que 1 e menores que 100
+
+num = 1
+
+while num < 100
+    puts num
+    num += 1
 end
