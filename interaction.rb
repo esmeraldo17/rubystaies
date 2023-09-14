c9840245a5aad3fedba9c933a5488ed4e70cfe6e
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 19:06:57 +0100

    study: extruturas de interacao - for

---
+++ interaction.rb
@@ -0,0 +1,13 @@
+#FOR
+
+#leia um array de elementos - linguagens de programacao
+
+linguagens = ["Ruby", "Go", "JavaScript"] 
+count = 1
+#intercao na lista com  laco for 
+
+for linguagem in linguagens
+    puts linguagem
+    puts count 
+    count = count +1
+end
