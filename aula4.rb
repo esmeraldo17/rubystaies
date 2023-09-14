e00c9acc8b7dd339d2ead1a1fef4526648aae493
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 17:10:29 +0100

    first commit

---
+++ aula4.rb
@@ -0,0 +1,20 @@
+#1) saida de dados na tela -->  aparecer um comando na tela
+#Usuarios digite seu nome
+
+print "Digite seu nome:  "
+
+
+#2) programa permita que o usuarios preencha o que foi pedido 
+#gets.chomp
+
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+
+sobrenome = gets.chomp
+
+
+#) saida final
+# para chmar um dados #{variavel}
+
+puts "Oi #{nome} #{sobrenome}"
---
+++ aula4soma.rb
@@ -0,0 +1,15 @@
+#usurio digite 1 numero
+#pedir ao usario 
+#.to_i
+print "Digite um numero inteiro:"
+num1 = gets.chomp.to_i
+
+#usuario digite outro numero
+print "Digite outro numero inteiro:"
+num2 = gets.chomp.to_i
+
+#some esses numero
+
+soma = num1 + num2
+
+puts "o resultado da soma sera: #{soma}"
