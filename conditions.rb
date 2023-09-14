a4a9e715d6a3c1d9276b1865b11b8a9a1e21fd91
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 18:51:37 +0100

    study: extruturas condicionais CASES

--- conditions.rb
+++ conditions.rb
@@ -22,10 +22,35 @@
 #UNLESS
 
 #declara minha variavel
-x=20
-    unless x > 15 # ao menos que x seja maior que 15
-        puts "x e maior ou igual a 15"
-    else
-        puts "x e maior que 15"
-    end
+# x=20
+#     unless x > 15 # ao menos que x seja maior que 15
+#         puts "x e maior ou igual a 15"
+#     else
+#         puts "x e maior que 15"
+#     end
 
+#CASE
+
+#usuario vais entra com o mes de nascimento dele
+#Analisar diversos CASES
+
+puts "Digite seu mes de nascimento:  "
+mes = gets.chomp.to_i
+
+#definir casos
+case mes
+when 1..3 #.. intervalo
+    puts "Voce nasceu no primeiro trimestre do ano"
+
+when 4..6 
+    puts "Voce nasceu no primeiro semestre do ano"
+
+when 7..9
+    puts "Voce nasceu no terceiro trimestre do ano"
+
+when 10..12
+    puts "Voce nasceu no segundo semestre do ano"
+
+else
+    puts "O valor digitado e invalido"
+end
