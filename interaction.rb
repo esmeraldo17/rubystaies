b10f18d53f186fffa394131674df94a404248bf6
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 19:26:53 +0100

    study: extruturas de interacao - times

--- interaction.rb
+++ interaction.rb
@@ -18,9 +18,32 @@
 #comparar um numero com uma condicao 
 #imprima todos os numeros maiores que 1 e menores que 100
 
-num = 1
+# num = 1
 
-while num < 100
-    puts num
-    num += 1
+# while num < 100
+#     puts num
+#     num += 1
+# end
+
+
+#TIMES
+
+#exemplo 1 puts
+6.times {puts "Oi"}
+
+#exemplo 1 print
+6.times {print "Oi "}
+
+#exemplo 2 puts
+4.times do 
+    puts "oi"
+end
+
+#exemplo 2 print
+4.times do
+    print "oi "
+end
+
+20.times do |contador|
+    puts "#{contador}"
 end
