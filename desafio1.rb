0a07ae3ae2ab76a189692a377a75771fb20c51ae
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 17:18:02 +0100

    feat: desafio1

---
+++ desafio1.rb
@@ -0,0 +1,10 @@
+print "Digite seu nome:  "
+nome = gets.chomp
+
+print "Digite seu sobrenome:  "
+nome = gets.chomp
+
+print "Digite a sua idade: "
+idade = gets.chomp
+
+puts "Leitor #{nome} #{sobrenome}, #{idade} anos!"
