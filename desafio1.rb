7562863182e571a02997f2c8719a4bf79cdc55b7
esmeraldo17 <esmeraldoalberto55@gmail.com>
2023-09-14 17:20:25 +0100

    chore: adicionando comentario

--- desafio1.rb
+++ desafio1.rb
@@ -2,9 +2,11 @@ print "Digite seu nome:  "
 nome = gets.chomp
 
 print "Digite seu sobrenome:  "
-nome = gets.chomp
+sobrenome = gets.chomp
 
 print "Digite a sua idade: "
 idade = gets.chomp
 
 puts "Leitor #{nome} #{sobrenome}, #{idade} anos!"
+
+#estudando saida e entrada de dados
